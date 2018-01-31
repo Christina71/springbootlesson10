c8ef736cf3a29f1e1921dc2a09e0449ae176049a
Christina <mariachristina71@yahoo.com>
2018-01-30 20:30:20 -0500

    Initial commit

---
+++ src/main/java/com/example/springbootlesson10/CourseRepository.java
@@ -0,0 +1,6 @@
+package com.example.springbootlesson10;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course,Long>{
+}
---
+++ src/main/java/com/example/springbootlesson10/Springbootlesson10Application.java
@@ -0,0 +1,12 @@
+package com.example.springbootlesson10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootlesson10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootlesson10Application.class, args);
+	}
+}
